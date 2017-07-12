cbf41a6fb3bc8dcea3134b2768b4b9392d7329bd
rohinieravi <rohinieravi@yahoo.com>
2017-07-12 13:31:15 -0700

    added test and CI

---
+++ test/test-server.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Hello world', function() {
+
+	before(function() {
+		return runServer();
+	});
+
+	after(function() {
+		return closeServer();
+	});
+
+	it('should return a html with status code 200', function() {
+		return chai.request(app)
+
+			.get('/')
+			.then(function(res) {
+				res.should.have.status(200);
+				res.should.be.html;
+			});
+	});
+});
